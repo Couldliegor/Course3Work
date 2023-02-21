7cb2144293c5a4aa11e324d8e705ea7f2bea00dd
egorcouldlie <couldliegor@gmail.com>
2023-02-21 12:12:12 +0300

    Master

---
+++ src/main/java/com/example/coursework/CourseworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkApplication.class, args);
+    }
+
+}
